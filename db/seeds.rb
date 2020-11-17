76a4fb2a5c1d25ba1a0af94d2655b56ad5e04e0c
thomasfarre <th.farre@gmail.com>
2020-11-17 09:52:32 +0100

    added a host user to seed and 2 monuments to test pundit

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@
 
 puts "Creating seeds...."
 
-user_images = ["https://avatars1.githubusercontent.com/u/71550958?s=400&u=5d9c2518ae08db91fa3742fff381ec6a364efd1d&v=4","https://avatars3.githubusercontent.com/u/61694823?s=460&u=41edf0546a675388bb39e187a44770b53bf500e0&v=4","https://avatars2.githubusercontent.com/u/71385272?s=460&u=d515496e33750499be8feae11fa4ce6a92613b3d&v=4"]
+user_images = ["https://avatars1.githubusercontent.com/u/71550958?s=400&u=5d9c2518ae08db91fa3742fff381ec6a364efd1d&v=4","https://avatars3.githubusercontent.com/u/61694823?s=460&u=41edf0546a675388bb39e187a44770b53bf500e0&v=4","https://avatars2.githubusercontent.com/u/71385272?s=460&u=d515496e33750499be8feae11fa4ce6a92613b3d&v=4", "https://cv.jpheos.fr/assets/images/jpheos.jpg"]
 
 monument_images = ["https://static.nationalgeographic.fr/files/styles/image_3200/public/babylonian-oasis-artist-rendering_0.jpg?w=1600","https://www.merveilles-du-monde.com/Sept/images/Jardins-de-Babylone/Jardins-de-Babylone-01.jpg", "https://www.merveilles-du-monde.com/Sept/images/Jardins-de-Babylone/Jardins-de-Babylone-02.jpg"]
 
@@ -27,19 +27,26 @@ monument_images = ["https://static.nationalgeographic.fr/files/styles/image_3200
 user1 = User.new(first_name: "Toto", last_name: "LeToto", email: "toto@toto.com", password: "123456", host: true)
 user2 = User.new(first_name: "Tata", last_name: "LaTata", email: "tata@toto.com", password: "123456", host: false)
 user3 = User.new(first_name: "Tutu", last_name: "LeTutu", email: "tutu@toto.com", password: "123456", host: false)
+user4 = User.new(first_name: "Titi", last_name: "LeTiti", email: "titi@toto.com", password: "123456", host: true)
+
 
 user1.photo.attach(io: URI.open(user_images[0]), filename: 'user.jpg', content_type: 'image/jpg')
 user2.photo.attach(io: URI.open(user_images[1]), filename: 'user.jpg', content_type: 'image/jpg')
 user3.photo.attach(io: URI.open(user_images[2]), filename: 'user.jpg', content_type: 'image/jpg')
+user4.photo.attach(io: URI.open(user_images[3]), filename: 'user.jpg', content_type: 'image/jpg')
 
 user1.save
 user2.save
 user3.save
+user4.save
 
 monument1 = Monument.new(name: "Les Jardins Suspendus", price_per_night: 7, description: "Les jardins suspendus de Babylone, c'est vraiment un chouette potager", average_rating: 5)
 monument2 = Monument.new(name: "Le Taj Mahal", price_per_night: 10, description: "Le Taj Mahal, petit monument sympatique", average_rating: 4)
 monument3 = Monument.new(name: "Chichen Itza", price_per_night: 8, description: "La plus chouette des pyramides!", average_rating: 4)
 monument4 = Monument.new(name: "Petra", price_per_night: 6, description: "Le desert, c'est super!", average_rating: 5)
+monument5 = Monument.new(name: "Machu Pichu", price_per_night: 9, description: "La montagne, ça vous gagne!", average_rating: 3)
+monument6 = Monument.new(name: "Colisée", price_per_night: 5, description: "En état de ruine depuis la dernière soirée", average_rating: 2)
+
 
 monument1.photos.attach(io: URI.open(monument_images[0]), filename: 'monument.jpg', content_type: 'image/jpg')
 monument1.photos.attach(io: URI.open(monument_images[1]), filename: 'monument.jpg', content_type: 'image/jpg')
@@ -57,17 +64,29 @@ monument4.photos.attach(io: URI.open("https://static.nationalgeographic.fr/files
 monument4.photos.attach(io: URI.open("https://www.les-covoyageurs.com/ressources/images-lieux/15-visiter-petra-jordanie.jpg"), filename: 'monument.jpg', content_type: 'image/jpg')
 monument4.photos.attach(io: URI.open("https://images4.bovpg.net/r/back/fr/sale/5f3f912809fa8o.jpg"), filename: 'monument.jpg', content_type: 'image/jpg')
 
+monument5.photos.attach(io: URI.open("https://cdn.getyourguide.com/img/tour/5d186c3f36cbf.jpeg/146.jpg"), filename: 'monument.jpg', content_type: 'image/jpg')
+monument5.photos.attach(io: URI.open("https://cdn.generationvoyage.fr/2019/11/sur-les-traces-du-machu-picchu-dans-les-nuages-perou.jpg"), filename: 'monument.jpg', content_type: 'image/jpg')
+monument5.photos.attach(io: URI.open("https://www.mademoiselle-voyage.fr/wp-content/uploads/2016/01/perou-machu-picchu-mademoiselle-voyage-1440x960.jpg"), filename: 'monument.jpg', content_type: 'image/jpg')
+
+monument6.photos.attach(io: URI.open("https://www.voyageway.com/wp-content/uploads/2012/11/visite-colisee-rome-1280x720.jpg"), filename: 'monument.jpg', content_type: 'image/jpg')
+monument6.photos.attach(io: URI.open("https://www.merveilles-du-monde.com/Colisee/images/Photos/Colisee4.jpg"), filename: 'monument.jpg', content_type: 'image/jpg')
+monument6.photos.attach(io: URI.open("https://www.batiactu.com/images/auto/620-465-c/20120828_145707_colosseuminromeitaly-april2007-diliff.jpg"), filename: 'monument.jpg', content_type: 'image/jpg')
+
+
 
 monument1.user = user1
 monument2.user = user1
 monument3.user = user1
 monument4.user = user1
+monument5.user = user4
+monument6.user = user4
 
 monument1.save
 monument2.save
 monument3.save
 monument4.save
-
+monument5.save
+monument6.save
 
 
 
