dbc1f230f03c727472734e1b58339bf591c83af7
Lior Levy <levy.lior.pro@gmail.com>
2020-11-17 15:26:46 +0100

    Add date_of_departure instance method to bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,13 @@
+require 'date'
+
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :monument
   has_many :reviews, dependent: :destroy
 
   validates :duration, presence: true
+
+  def date_of_departure
+    self.date_of_arrival += duration
+  end
 end
